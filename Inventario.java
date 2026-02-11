ee653331f08f0ac8f80e7f4ea33c34cbf514a4b9
samantacastillo2-pixel <samanta.castillo2@ujcv.edu.hn>
2026-02-10 23:32:32 -0600

    Update Inventario.java

--- Inventario.java
+++ Inventario.java
@@ -1,21 +1,27 @@
 import java.util.ArrayList;
+
 // clase que se encarga de administrar la lista de productos
 public class Inventario {
+     
      // lista donde se guardan los productos del inventario
     private ArrayList<Producto> productos;
-    // constructor que inicializa la lista al crear el inventario
+    
+     // constructor que inicializa la lista al crear el inventario
     public Inventario() {
         productos = new ArrayList<>();
     }
-    // metodo para añadir un nuevo producto a la lista
+    
+     // metodo para añadir un nuevo producto a la lista
     public void agregarProducto(Producto p) {
         productos.add(p);
     }
-    // metodo que elimina un producto segun su codigo
+    
+     // metodo que elimina un producto segun su codigo
     // devuelve true si se elimino correctamente
     public boolean eliminarProducto(String codigo) {
         return productos.removeIf(p -> p.getCodigo().equalsIgnoreCase(codigo));
     }
+     
      // metodo que busca un producto por su codigo
     // si lo encuentra lo devuelve, si no retorna null
     public Producto buscarProducto(String codigo) {
@@ -26,7 +32,8 @@ public class Inventario {
         }
         return null;
     }
-    // metodo que muestra todos los productos almacenados
+    
+     // metodo que muestra todos los productos almacenados
     public void listarProductos() {
         if (productos.isEmpty()) {
             System.out.println("Inventario vacío");
@@ -37,4 +44,5 @@ public class Inventario {
         }
     }
 }
+
 
