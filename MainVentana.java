649e4fd858db5fa6fa4805ae762cb72a8725a7ae
Abril Andino <abril21.aa@gmail.com>
2026-03-12 09:37:38 -0600

    Create MainVentana.java

---
+++ MainVentana.java
@@ -0,0 +1,10 @@
+public class MainVentana {
+
+    public static void main(String[] args) {
+
+        VentanaInventario ventana = new VentanaInventario();
+        ventana.setVisible(true);
+
+    }
+
+}
