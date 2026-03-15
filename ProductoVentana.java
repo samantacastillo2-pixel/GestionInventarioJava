da58c833c06068226d76d6b560826df8cb87e64a
samantacastillo2-pixel <samanta.castillo2@ujcv.edu.hn>
2026-03-15 10:13:39 -0600

    Create ProductoVentana.java

---
+++ ProductoVentana.java
@@ -0,0 +1,47 @@
+public class ProductoVentana {
+
+    private String codigo;
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public ProductoVentana(String codigo, String nombre, int cantidad, double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
